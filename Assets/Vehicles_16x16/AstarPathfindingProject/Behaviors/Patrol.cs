cc70b39dce7869cac34d44ed093323bd471205c8
StarmonkVN332 <starmonkvn332@gmail.com>
2024-07-26 11:39:45 +0700

    Vehicle
    Vehicle AI

---
+++ Assets/Vehicles_16x16/AstarPathfindingProject/Behaviors/Patrol.cs
@@ -0,0 +1,309 @@
+//using UnityEngine;
+//using System.Collections;
+
+//namespace Pathfinding {
+//	/// <summary>
+//	/// Simple patrol behavior.
+//	/// This will set the destination on the agent so that it moves through the sequence of objects in the <see cref="targets"/> array.
+//	/// Upon reaching a target it will wait for <see cref="delay"/> seconds.
+//	///
+//	/// See: <see cref="Pathfinding.AIDestinationSetter"/>
+//	/// See: <see cref="Pathfinding.AIPath"/>
+//	/// See: <see cref="Pathfinding.RichAI"/>
+//	/// See: <see cref="Pathfinding.AILerp"/>
+//	/// </summary>
+//	[UniqueComponent(tag = "ai.destination")]
+//	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_patrol.php")]
+//	public class Patrol : VersionedMonoBehaviour {
+//		/// <summary>Target points to move to in order</summary>
+//		public Transform[] targets;
+
+//		/// <summary>Time in seconds to wait at each target</summary>
+//		public float delay = 0;
+
+//		/// <summary>Current target index</summary>
+//		int index;
+
+//		IAstarAI agent;
+//		float switchTime = float.PositiveInfinity;
+
+//		protected override void Awake () {
+//			base.Awake();
+//			agent = GetComponent<IAstarAI>();
+//		}
+
+//		/// <summary>Update is called once per frame</summary>
+//		void Update () {
+//			if (targets.Length == 0) return;
+
+//			bool search = false;
+
+//			// Note: using reachedEndOfPath and pathPending instead of reachedDestination here because
+//			// if the destination cannot be reached by the agent, we don't want it to get stuck, we just want it to get as close as possible and then move on.
+//			if (agent.reachedEndOfPath && !agent.pathPending && float.IsPositiveInfinity(switchTime)) {
+//				switchTime = Time.time + delay;
+//			}
+
+//			if (Time.time >= switchTime) {
+//				index = index + 1;
+//				search = true;
+//				switchTime = float.PositiveInfinity;
+//			}
+
+//			index = index % targets.Length;
+//			agent.destination = targets[index].position;
+
+//			if (search) agent.SearchPath();
+//		}
+//	}
+//}
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+namespace Pathfinding
+{
+    [UniqueComponent(tag = "ai.destination")]
+    [HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_patrol.php")]
+    public class Patrol : VersionedMonoBehaviour
+    {
+        public Transform[] waypoints;
+        public float delay = 0;
+        public float detectionDistance = 2f; // Distance to detect cars ahead
+        public LayerMask carLayerMask; // Layer mask for car detection
+        public float slowDownFactor = 0.5f; // Factor to slow down the car when an obstacle is detected
+        public float checkInterval = 0.2f; // Time interval for obstacle checks
+        public float intersectionStopDuration = 1f; // Duration to stop at an intersection
+        private bool isWaiting = false;
+
+        private int index = 0;
+        private IAstarAI agent;
+        private Seeker seeker;
+        private float switchTime = float.PositiveInfinity;
+        private bool isSlowedDown = false;
+        private float nextCheckTime = 0f;
+        private float originalSpeed;
+        private bool isStoppingAtIntersection = false;
+
+        public SpriteSet currentSpriteSet;
+        public AnimationSet currentAnimationSet;
+        public CarColliderConfig leftRightCarColliderConfig;
+        public CarColliderConfig upDownCarColliderConfig;
+
+        private Animator animator;
+        private SpriteRenderer spriteRenderer;
+        private BoxCollider2D carCollider;
+        private Vector3 previousPosition;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            agent = GetComponent<IAstarAI>();
+            seeker = GetComponent<Seeker>();
+            animator = GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            carCollider = GetComponent<BoxCollider2D>();
+            previousPosition = transform.position;
+        }
+
+        void Start()
+        {
+            originalSpeed = agent.maxSpeed;
+        }
+
+        void Update()
+        {
+            if (waypoints.Length == 0) return;
+
+            if (Time.time >= nextCheckTime)
+            {
+                nextCheckTime = Time.time + checkInterval;
+
+                if (CheckForObstaclesAlongPath())
+                {
+                    SlowDown();
+                }
+                else if (isSlowedDown)
+                {
+                    ResumeNormalSpeed();
+                }
+            }
+
+            if (isStoppingAtIntersection)
+            {
+                return; // Skip movement if stopping at intersection
+            }
+
+            bool search = false;
+
+            if (agent.reachedEndOfPath && !agent.pathPending && float.IsPositiveInfinity(switchTime))
+            {
+                switchTime = Time.time + delay;
+            }
+
+            if (Time.time >= switchTime)
+            {
+                NextWaypoint();
+                search = true;
+                switchTime = float.PositiveInfinity;
+            }
+
+            UpdateDirectionAndAnimation();
+
+            agent.destination = waypoints[index].position;
+
+            if (search) agent.SearchPath();
+
+            previousPosition = transform.position;
+        }
+
+        void NextWaypoint()
+        {
+            index = (index + 1) % waypoints.Length;
+        }
+
+        void UpdateDirectionAndAnimation()
+        {
+            Vector3 currentVelocity = (transform.position - previousPosition) / Time.deltaTime;
+
+            if (Mathf.Abs(currentVelocity.x) > Mathf.Abs(currentVelocity.y))
+            {
+                if (currentVelocity.x > 0)
+                {
+                    Debug.Log("Right");
+                    UpdateCarState(currentSpriteSet.rightSprite, currentAnimationSet.rightClip.name, leftRightCarColliderConfig);
+                }
+                else if (currentVelocity.x < 0)
+                {
+                    Debug.Log("Left");
+                    UpdateCarState(currentSpriteSet.leftSprite, currentAnimationSet.leftClip.name, leftRightCarColliderConfig);
+                }
+            }
+            else
+            {
+                if (currentVelocity.y > 0)
+                {
+                    Debug.Log("Up");
+                    UpdateCarState(currentSpriteSet.upSprite, currentAnimationSet.upClip.name, upDownCarColliderConfig);
+                }
+                else if (currentVelocity.y < 0)
+                {
+                    Debug.Log("Down");
+                    UpdateCarState(currentSpriteSet.downSprite, currentAnimationSet.downClip.name, upDownCarColliderConfig);
+                }
+            }
+        }
+        public void AllowToProceed()
+        {
+            isWaiting = false;
+            agent.isStopped = false;
+            // Resume normal movement
+        }
+        void UpdateCarState(Sprite sprite, string animationClipName, CarColliderConfig colliderConfig)
+        {
+            spriteRenderer.sprite = sprite;
+            animator.Play(animationClipName);
+            AdjustCarColliderSizeAndOffset(colliderConfig);
+        }
+
+        void AdjustCarColliderSizeAndOffset(CarColliderConfig config)
+        {
+            if (carCollider != null)
+            {
+                carCollider.size = config.size;
+                carCollider.offset = config.offset;
+            }
+        }
+
+        bool CheckForObstaclesAlongPath()
+        {
+            var path = seeker.GetCurrentPath();
+            if (path != null && path.vectorPath != null)
+            {
+                foreach (var point in path.vectorPath)
+                {
+                    Collider2D hit = Physics2D.OverlapCircle(point, detectionDistance, carLayerMask);
+                    if (hit != null && hit.transform != transform)
+                    {
+                        Debug.Log("Obstacle detected at point: " + point + " Obstacle: " + hit.transform.name);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        void SlowDown()
+        {
+            if (!isSlowedDown)
+            {
+                Debug.Log("Slowing down");
+                agent.maxSpeed = originalSpeed * slowDownFactor;
+                isSlowedDown = true;
+            }
+        }
+
+        void ResumeNormalSpeed()
+        {
+            if (isSlowedDown)
+            {
+                Debug.Log("Resuming normal speed");
+                agent.maxSpeed = originalSpeed;
+                isSlowedDown = false;
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Intersection"))
+            {
+                StartCoroutine(HandleIntersection());
+            }
+        }
+
+        IEnumerator HandleIntersection()
+        {
+            isStoppingAtIntersection = true;
+            Debug.Log("Stopped at intersection");
+
+            // Stop for a fixed duration
+            yield return new WaitForSeconds(intersectionStopDuration);
+
+            // Check if there are other cars at the intersection
+            // This should be your logic to determine who should go first
+            // For now, just assume it is okay to continue after stopping
+            isStoppingAtIntersection = false;
+
+            Debug.Log("Continuing after intersection");
+        }
+    }
+
+    [System.Serializable]
+    public class CarColliderConfig
+    {
+        public Vector2 size;
+        public Vector2 offset;
+    }
+
+    [System.Serializable]
+    public class SpriteSet
+    {
+        public Sprite leftSprite;
+        public Sprite rightSprite;
+        public Sprite upSprite;
+        public Sprite downSprite;
+    }
+
+    [System.Serializable]
+    public class AnimationSet
+    {
+        public AnimationClip leftClip;
+        public AnimationClip rightClip;
+        public AnimationClip upClip;
+        public AnimationClip downClip;
+    }
+}
+
+
---
+++ Assets/Vehicles_16x16/Intersection.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+public class Intersection : MonoBehaviour
+{
+    public LayerMask carLayerMask;
+    public float intersectionWaitTime = 1f; // Time for cars to wait at the intersection
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            StartCoroutine(HandleCarAtIntersection(other));
+        }
+    }
+
+    private IEnumerator HandleCarAtIntersection(Collider2D car)
+    {
+        // Wait for 1 second before processing
+        yield return new WaitForSeconds(intersectionWaitTime);
+
+        // Check if the car is still in the intersection
+        if (IsCarInIntersection(car))
+        {
+            // Determine the order in which cars should go
+            DetermineCarPriority();
+        }
+    }
+
+    private bool IsCarInIntersection(Collider2D car)
+    {
+        // Check if the car is within the intersection bounds
+        // Assuming the intersection has a BoxCollider2D to represent its area
+        Collider2D intersectionCollider = GetComponent<Collider2D>();
+        return intersectionCollider.IsTouching(car);
+    }
+
+    private void DetermineCarPriority()
+    {
+        // Retrieve all cars currently in the intersection
+        Collider2D[] carsInIntersection = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0, carLayerMask);
+
+        // Sort cars based on their entry time into the intersection
+        List<Collider2D> sortedCars = new List<Collider2D>(carsInIntersection);
+        sortedCars.Sort((car1, car2) => car1.GetComponent<Car>().entryTime.CompareTo(car2.GetComponent<Car>().entryTime));
+
+        // Allow cars to proceed based on their priority
+        foreach (Collider2D car in sortedCars)
+        {
+            car.GetComponent<Patrol>().AllowToProceed();
+        }
+    }
+}
+
+public class Car : MonoBehaviour
+{
+    public float stopDuration = 1f; // Time for the car to stop at the intersection
+    public float detectionDistance = 2f;
+    public LayerMask carLayerMask;
+
+    private IAstarAI agent;
+    public float entryTime;
+    private bool isWaiting = false;
+
+    private void Awake()
+    {
+        agent = GetComponent<IAstarAI>();
+    }
+
+    private void Update()
+    {
+        if (isWaiting)
+        {
+            return; // Skip movement while waiting
+        }
+
+        // Existing code to handle movement and slowing down
+        
+
+        
+    }
+    private void SlowDown()
+    {
+        agent.maxSpeed = agent.maxSpeed * 0.5f; // Slow down factor
+    }
+
+    private void ResumeNormalSpeed()
+    {
+        agent.maxSpeed = agent.maxSpeed / 0.5f; // Resume original speed
+    }
+    
+    public void StopAtIntersection(float waitTime)
+    {
+        entryTime = Time.time;
+        isWaiting = true;
+        agent.isStopped = true;
+        Invoke("AllowToProceed", waitTime); // Allow the car to proceed after wait time
+    }
+
+    public void AllowToProceed()
+    {
+        isWaiting = false;
+        agent.isStopped = false;
+        // Resume normal movement
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Intersection"))
+        {
+            StopAtIntersection(other.GetComponent<Intersection>().intersectionWaitTime);
+        }
+    }
+}
